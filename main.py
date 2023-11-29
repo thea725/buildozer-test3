f153381bbb637b29fbf0aa9580de611a46ab7f94
M Rifki Muchtar <91528096+thea725@users.noreply.github.com>
2023-11-29 16:20:41 +0700

    test cam

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
+import kivy
 from kivy.app import App
-from kivy.uix.image import Image
-from kivy.clock import Clock
-from kivy.graphics.texture import Texture
+from kivy.uix.label import Label
 import cv2
-
-class KivyCamera(Image):
-    def __init__(self, capture, fps, **kwargs):
-        super(KivyCamera, self).__init__(**kwargs)
-        self.capture = capture
-        Clock.schedule_interval(self.update, 1.0 / fps)
-
-    def update(self, dt):
-        ret, frame = self.capture.read()
-        if ret:
-            # convert it to texture
-            buf1 = cv2.flip(frame, 0)
-            buf = buf1.tostring()
-            image_texture = Texture.create(
-                size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
-            image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
-            # display image from the texture
-            self.texture = image_texture
-
-
-class CamApp(App):
+import numpy as np
+from multiprocessing import Process
+  
+class MyLabelApp(App):
     def build(self):
-        self.capture = cv2.VideoCapture("./vid4.mp4")
-        self.my_camera = KivyCamera(capture=self.capture, fps=30)
-        return self.my_camera
-
-    def on_stop(self):
-        #without this, app will not exit even if the window is closed
-        self.capture.release()
-
-
-if __name__ == '__main__':
-    CamApp().run()
+        caption = cv2.VideoCapture(0,cv2.CAP_V4L2)
+        if not caption.isOpened():
+            lbl2 = Label(text ="cannot open camera 0")
+            return lbl2
+            caption.release()
+        else:
+            caption.release()
+        caption1 = cv2.VideoCapture(1,cv2.CAP_V4L2)
+        if not caption1.isOpened():
+            lbl3 = Label(text ="cannot open camera 1")
+            return lbl3
+            caption1.release()
+        else:
+            caption1.release()
+        lbl = Label(text ="Android Two Cam Is running, Close window and save ouput.avi locally!!! ")
+        return lbl
+def cap():
+    cap = cv2.VideoCapture(0,cv2.CAP_V4L2)
+    cap1 = cv2.VideoCapture(1,cv2.CAP_V4L2)
+    out = cv2.VideoWriter('/storage/emulated/0/dcim/output.avi',cv2.VideoWriter_fourcc(*'XVID'), cap.get(cv2.CAP_PROP_FPS), (1440,720))
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        ret1, frame1 = cap1.read()
+        if ret == True:
+            reframe = cv2.resize(frame,(720, 720), interpolation = cv2.INTER_AREA)
+            reframe1 = cv2.resize(frame1,(720, 720), interpolation = cv2.INTER_AREA)
+            both = np.column_stack((reframe, reframe1))
+            out.write(both) 
+if __name__ == "__main__":
+    p1 = Process(target=cap)
+    p1.start()
+    label = MyLabelApp()
+    label.run()
+    label.on_stop(p1.terminate())
