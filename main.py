6c284a77b2e224324f2d65178c624c195602ed17
M Rifki Muchtar <91528096+thea725@users.noreply.github.com>
2023-12-01 07:03:03 +0700

    enhancer only

--- main.py
+++ main.py
@@ -15,26 +15,19 @@ def platform():
     elif _sys_platform in ('win32', 'cygwin'):
         return "win"
 
-def defisheye(img):
-    DIM = img.shape[:2][::-1]
-    balance = 0
-    K = np.array([[1122.4054962744387, 0.0, 1006.1145835723129], [0.0, 1129.0933478170655, 527.4670240270237], [0.0, 0.0, 1.0]])
-    D = np.array([[-0.21503184621950375], [0.7441653867540186], [-1.3654196840660953], [0.8759864071569387]])
-    dim1 = [1920, 1080]
-    dim2 = [1920, 1080]
-    dim3 = [1920, 1080]
+def normalization(img):
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    normalized_image = cv2.normalize(gray_image, None, 0, 255, cv2.NORM_MINMAX)
+    clahe = cv2.createCLAHE(clipLimit=10.0, tileGridSize=(20, 20))
+    enhanced_image = clahe.apply(normalized_image)
+    equ = cv2.equalizeHist(enhanced_image)
+    
+    upper_black = 75
+    equ[np.where(equ <= upper_black)] = 0
+    equ[np.where(equ <= upper_black)] = 0
 
-    assert dim1[0]/dim1[1] == DIM[0]/DIM[1], "Image to undistort needs to have same aspect ratio as the ones used in calibration"
+    return equ
 
-    scaled_K = K * dim1[0] / DIM[0]  # The values of K is to scale with image dimension.
-    scaled_K[2][2] = 1.0
-
-    new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(scaled_K, D, dim2, np.eye(3), balance=balance, fov_scale=1)
-    map1, map2 = cv2.fisheye.initUndistortRectifyMap(scaled_K, D, np.eye(3), new_K, dim3, cv2.CV_16SC2)
-
-    dst = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
-
-    return dst
 class ImageApp(App):
     def build(self):
         self.layout = BoxLayout(orientation='vertical')
@@ -57,15 +50,16 @@ class ImageApp(App):
         ret, frame = self.cap.read()
         if not ret:
             return
-
-        # Ubah warna dari BGR ke RGB
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # normal = defisheye(frame)
+        result = normalization(frame)
+        # result = edge_detection(normal, enhance)
         
-        normal = defisheye(frame_rgb)
+        # Ubah warna dari BGR ke RGB
+        frame_rgb = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
 
         # Buat tekstur Kivy dari citra OpenCV
-        texture = Texture.create(size=(normal.shape[1], normal.shape[0]), colorfmt='rgb')
-        texture.blit_buffer(cv2.flip(normal, 0).tostring(), colorfmt='rgb', bufferfmt='ubyte')
+        texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='rgb')
+        texture.blit_buffer(cv2.flip(frame_rgb, 0).tobytes(), colorfmt='rgb', bufferfmt='ubyte')
 
         # Tampilkan gambar di aplikasi
         self.image.texture = texture
