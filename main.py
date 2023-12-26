a9e970507a9e6ae75251f310cdad61aee6e832f9
M Rifki Muchtar <91528096+thea725@users.noreply.github.com>
2023-12-26 16:07:04 +0700

    ultralytics test

--- main.py
+++ main.py
@@ -1,273 +1,48 @@
-import tensorflow as tf
-import numpy as np
-from PIL import Image
-from io import BytesIO
-import matplotlib.pyplot as plt
-import time
-import collections
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.graphics.texture import Texture
+import cv2
+from kivy.clock import Clock
+from ultralytics import YOLO
 
-# Definisikan fungsi load_image_into_numpy_array dan run_inference_for_single_image di sini
+import os
+from sys import platform as _sys_platform
 
-# Path ke model TFLite
-_TFLITE_MODEL_PATH = "model.tflite"
+model = YOLO('best.pt')
 
-# Load model TFLite
-interpreter = tf.lite.Interpreter(model_path=_TFLITE_MODEL_PATH)
-interpreter.allocate_tensors()
+class ImageApp(App):
+    def build(self):
+        self.layout = BoxLayout(orientation='vertical')
+        self.image = Image()
+        self.layout.add_widget(self.image)
 
-# Fungsi untuk melakukan inferensi menggunakan model TFLite
-def run_inference_tflite(interpreter, image):
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
+        path = os.path.join(os.getcwd(), 'vid4.mp4')
+        self.cap = cv2.VideoCapture(path)
 
-    # Masukkan gambar ke input tensor model
-    interpreter.set_tensor(input_details[0]['index'], image)
-    interpreter.invoke()
+        # Atur pemanggilan fungsi update setiap 1/30 detik
+        Clock.schedule_interval(self.update, 1.0 / 30.0)
 
-    # Dapatkan hasil inferensi dari output tensor model
-    output_dict = {}
-    for i, detail in enumerate(output_details):
-        output_dict[detail['name']] = interpreter.get_tensor(detail['index'])
+        return self.layout
 
-    return output_dict
+    def update(self, *args):
+        # Baca gambar menggunakan OpenCV
+        ret, frame = self.cap.read()
+        if not ret:
+            return
 
-# Path ke gambar yang akan diuji
-image_path = "frame_0000_jpg.rf.a8eb77de7eae2d60b7ae0aca794b041e.jpg"
+        # Ubah warna dari BGR ke RGB
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-def load_image_into_numpy_array(path):
-    """Load an image from file into a numpy array.  
-    Puts image into numpy array to feed into tensorflow graph.
-    Note that by convention we put it into a numpy array with shape
-    (height, width, channels), where channels=3 for RGB.    
-    Args:
-      path: a file path (this can be local or on colossus)  
-    Returns:
-      uint8 numpy array with shape (img_height, img_width, 3)
-    """
-    img_data = tf.io.gfile.GFile(path, 'rb').read()
-    image = Image.open(BytesIO(img_data))
-    (im_width, im_height) = image.size
-    return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
+        results = model(frame_rgb)
+        results = results[0].plot()
 
-# Load gambar ke dalam array numpy
-image_np = load_image_into_numpy_array(image_path)
+        # Buat tekstur Kivy dari citra OpenCV
+        texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='rgb')
+        texture.blit_buffer(cv2.flip(results, 0).tostring(), colorfmt='rgb', bufferfmt='ubyte')
 
-# Persiapkan gambar untuk input model TFLite
+        # Tampilkan gambar di aplikasi
+        self.image.texture = texture
 
-input_image = tf.image.resize(image_np, (320, 320))
-input_image = np.expand_dims(input_image, axis=0)
-input_image = input_image.astype(np.float32)
-
-# Lakukan inferensi menggunakan model TFLite
-start_time = time.time()
-output_dict_tflite = run_inference_tflite(interpreter, input_image)
-end_time = time.time()
-
-def visualize_boxes_and_labels_on_image_array(
-    image,
-    boxes,
-    classes,
-    scores,
-    category_index,
-    instance_masks=None,
-    instance_boundaries=None,
-    keypoints=None,
-    keypoint_scores=None,
-    keypoint_edges=None,
-    track_ids=None,
-    use_normalized_coordinates=False,
-    max_boxes_to_draw=20,
-    min_score_thresh=.5,
-    agnostic_mode=False,
-    line_thickness=4,
-    mask_alpha=.4,
-    groundtruth_box_visualization_color='black',
-    skip_boxes=False,
-    skip_scores=False,
-    skip_labels=False,
-    skip_track_ids=False):
-  """Overlay labeled boxes on an image with formatted scores and label names.
-
-  This function groups boxes that correspond to the same location
-  and creates a display string for each detection and overlays these
-  on the image. Note that this function modifies the image in place, and returns
-  that same image.
-
-  Args:
-    image: uint8 numpy array with shape (img_height, img_width, 3)
-    boxes: a numpy array of shape [N, 4]
-    classes: a numpy array of shape [N]. Note that class indices are 1-based,
-      and match the keys in the label map.
-    scores: a numpy array of shape [N] or None.  If scores=None, then
-      this function assumes that the boxes to be plotted are groundtruth
-      boxes and plot all boxes as black with no classes or scores.
-    category_index: a dict containing category dictionaries (each holding
-      category index `id` and category name `name`) keyed by category indices.
-    instance_masks: a uint8 numpy array of shape [N, image_height, image_width],
-      can be None.
-    instance_boundaries: a numpy array of shape [N, image_height, image_width]
-      with values ranging between 0 and 1, can be None.
-    keypoints: a numpy array of shape [N, num_keypoints, 2], can
-      be None.
-    keypoint_scores: a numpy array of shape [N, num_keypoints], can be None.
-    keypoint_edges: A list of tuples with keypoint indices that specify which
-      keypoints should be connected by an edge, e.g. [(0, 1), (2, 4)] draws
-      edges from keypoint 0 to 1 and from keypoint 2 to 4.
-    track_ids: a numpy array of shape [N] with unique track ids. If provided,
-      color-coding of boxes will be determined by these ids, and not the class
-      indices.
-    use_normalized_coordinates: whether boxes is to be interpreted as
-      normalized coordinates or not.
-    max_boxes_to_draw: maximum number of boxes to visualize.  If None, draw
-      all boxes.
-    min_score_thresh: minimum score threshold for a box or keypoint to be
-      visualized.
-    agnostic_mode: boolean (default: False) controlling whether to evaluate in
-      class-agnostic mode or not.  This mode will display scores but ignore
-      classes.
-    line_thickness: integer (default: 4) controlling line width of the boxes.
-    mask_alpha: transparency value between 0 and 1 (default: 0.4).
-    groundtruth_box_visualization_color: box color for visualizing groundtruth
-      boxes
-    skip_boxes: whether to skip the drawing of bounding boxes.
-    skip_scores: whether to skip score when drawing a single detection
-    skip_labels: whether to skip label when drawing a single detection
-    skip_track_ids: whether to skip track id when drawing a single detection
-
-  Returns:
-    uint8 numpy array with shape (img_height, img_width, 3) with overlaid boxes.
-  """
-  # Create a display string (and color) for every box location, group any boxes
-  # that correspond to the same location.
-  box_to_display_str_map = collections.defaultdict(list)
-  box_to_color_map = collections.defaultdict(str)
-  box_to_instance_masks_map = {}
-  box_to_instance_boundaries_map = {}
-  box_to_keypoints_map = collections.defaultdict(list)
-  box_to_keypoint_scores_map = collections.defaultdict(list)
-  box_to_track_ids_map = {}
-  if not max_boxes_to_draw:
-    max_boxes_to_draw = boxes.shape[0]
-  for i in range(boxes.shape[0]):
-    if max_boxes_to_draw == len(box_to_color_map):
-      break
-    if scores is None or scores[i] > min_score_thresh:
-      box = tuple(boxes[i].tolist())
-      if instance_masks is not None:
-        box_to_instance_masks_map[box] = instance_masks[i]
-      if instance_boundaries is not None:
-        box_to_instance_boundaries_map[box] = instance_boundaries[i]
-      if keypoints is not None:
-        box_to_keypoints_map[box].extend(keypoints[i])
-      if keypoint_scores is not None:
-        box_to_keypoint_scores_map[box].extend(keypoint_scores[i])
-      if track_ids is not None:
-        box_to_track_ids_map[box] = track_ids[i]
-      if scores is None:
-        box_to_color_map[box] = groundtruth_box_visualization_color
-      else:
-        display_str = ''
-        if not skip_labels:
-          if not agnostic_mode:
-            if classes[i] in six.viewkeys(category_index):
-              class_name = category_index[classes[i]]['name']
-            else:
-              class_name = 'N/A'
-            display_str = str(class_name)
-        if not skip_scores:
-          if not display_str:
-            display_str = '{}%'.format(round(100*scores[i]))
-          else:
-            display_str = '{}: {}%'.format(display_str, round(100*scores[i]))
-        if not skip_track_ids and track_ids is not None:
-          if not display_str:
-            display_str = 'ID {}'.format(track_ids[i])
-          else:
-            display_str = '{}: ID {}'.format(display_str, track_ids[i])
-        box_to_display_str_map[box].append(display_str)
-        if agnostic_mode:
-          box_to_color_map[box] = 'DarkOrange'
-        elif track_ids is not None:
-          prime_multipler = _get_multiplier_for_color_randomness()
-          box_to_color_map[box] = STANDARD_COLORS[
-              (prime_multipler * track_ids[i]) % len(STANDARD_COLORS)]
-        else:
-          box_to_color_map[box] = STANDARD_COLORS[
-              classes[i] % len(STANDARD_COLORS)]
-
-  # Draw all boxes onto image.
-  for box, color in box_to_color_map.items():
-    ymin, xmin, ymax, xmax = box
-    if instance_masks is not None:
-      draw_mask_on_image_array(
-          image,
-          box_to_instance_masks_map[box],
-          color=color,
-          alpha=mask_alpha
-      )
-    if instance_boundaries is not None:
-      draw_mask_on_image_array(
-          image,
-          box_to_instance_boundaries_map[box],
-          color='red',
-          alpha=1.0
-      )
-    draw_bounding_box_on_image_array(
-        image,
-        ymin,
-        xmin,
-        ymax,
-        xmax,
-        color=color,
-        thickness=0 if skip_boxes else line_thickness,
-        display_str_list=box_to_display_str_map[box],
-        use_normalized_coordinates=use_normalized_coordinates)
-    if keypoints is not None:
-      keypoint_scores_for_box = None
-      if box_to_keypoint_scores_map:
-        keypoint_scores_for_box = box_to_keypoint_scores_map[box]
-      draw_keypoints_on_image_array(
-          image,
-          box_to_keypoints_map[box],
-          keypoint_scores_for_box,
-          min_score_thresh=min_score_thresh,
-          color=color,
-          radius=line_thickness / 2,
-          use_normalized_coordinates=use_normalized_coordinates,
-          keypoint_edges=keypoint_edges,
-          keypoint_edge_color=color,
-          keypoint_edge_width=line_thickness // 2)
-
-  return image
-
-category_index = {0: {'id': 0, 'name': 'object'}}
-
-
-# print('---------------------------------------------')
-# print(output_dict_tflite["StatefulPartitionedCall:1"])
-# print('---------------------------------------------')
-# print(output_dict_tflite["StatefulPartitionedCall:3"])
-# print('---------------------------------------------')
-# print(output_dict_tflite["StatefulPartitionedCall:0"])
-# print('---------------------------------------------')
-# print(output_dict_tflite["StatefulPartitionedCall:2"])
-# print('---------------------------------------------')
-# Visualisasikan hasil inferensi
-visualize_boxes_and_labels_on_image_array(
-    image_np,
-    output_dict_tflite['StatefulPartitionedCall:3'][0],
-    output_dict_tflite['StatefulPartitionedCall:2'][0].astype(np.int64),
-    output_dict_tflite['StatefulPartitionedCall:1'][0],
-    category_index,
-    instance_masks=output_dict_tflite.get('detection_masks_reframed', None),
-    use_normalized_coordinates=True,
-    line_thickness=8
-)
-
-# Tampilkan gambar hasil inferensi
-plt.imshow(image_np)
-plt.show()
-# display(Image.fromarray(image_np))
-
-# Hitung dan tampilkan waktu eksekusi
-print("The time of execution of the program is:", (end_time - start_time) * 1000, "ms")
+if __name__ == '__main__':
+    ImageApp().run()
